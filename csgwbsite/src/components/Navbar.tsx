6c43d80ca4cdd35c0b34b73541a5e6d7af40c76e
SageCT <egas6554@gmail.com>
2023-08-11 18:52:35 -0500

    feat: Added separate component files

--- csgwbsite/src/components/Navbar.tsx
+++ csgwbsite/src/components/Navbar.tsx
@@ -1,35 +1,40 @@
+import React, { useState } from "react";
 import { themify } from "react-icons/tfi";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
   return (
     <>
-      <ul className="">
-        <div className="w-full text-lg flex flex-row justify-between">
-          <li>
-            <a href="#" className="text-white hover:text-gray-300">
-              About
-            </a>
-          </li>
+      <div className="flex flex-row ">
+        <ul className="">
+          <div className="w-full h-24 max-w-[1240px] mx-auto text-lg flex flex-row justify-between">
+            <li>
+              <a href="#" className="text-white p-4 hover:text-gray-300">
+                About
+              </a>
+            </li>
 
-          <li>
-            <a href="#" className="text-white">
-              Officers
-            </a>
-          </li>
+            <li>
+              <a href="#" className="text-white p-4">
+                Officers
+              </a>
+            </li>
 
-          <li>
-            <a href="#" className="text-white">
-              Events
-            </a>
-          </li>
+            <li>
+              <a href="#" className="text-white p-4">
+                Events
+              </a>
+            </li>
 
-          <li>
-            <a href="#" className="text-white">
-              Contact Us
-            </a>
-          </li>
-        </div>
-      </ul>
+            <li>
+              <a href="#" className="text-white p-4">
+                Contact Us
+              </a>
+            </li>
+          </div>
+        </ul>
+      </div>
     </>
   );
 };
