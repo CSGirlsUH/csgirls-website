9a29272ab0e2435332b5ba62b2a46b5e615d7738
SageCT <egas6554@gmail.com>
2023-09-30 15:49:20 -0500

    Started work on About.tsx

--- csgwebsite/src/pages/About.tsx
+++ csgwebsite/src/pages/About.tsx
@@ -1,8 +1,20 @@
 // import Navbar from "./components/Navbar";
 // import Card from "./components/Card";
 
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import AboutMain from "../components/AboutMain";
+
 function About() {
-  return <div>About</div>;
+  return (
+    <>
+      <div>
+        <Navbar />
+        <AboutMain />
+        <Footer />
+      </div>
+    </>
+  );
 }
 
 export default About;
