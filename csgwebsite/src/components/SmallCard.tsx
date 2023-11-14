bf8214eda445b4836aa521ba55ed6fce9d7c6a4e
SageCT <99381480+SageCT@users.noreply.github.com>
2023-11-14 16:47:26 -0600

    Continued work on elongated SmallCard

--- csgwebsite/src/components/SmallCard.tsx
+++ csgwebsite/src/components/SmallCard.tsx
@@ -3,12 +3,19 @@ import React from "react";
 interface CardProps {
   title: string;
   pText: string;
+  elongated?: boolean;
+  image?: string;
 }
 
 const SmallCard = (props: CardProps) => {
   return (
     <>
-      <div className="w-32 h-32 bg-bggray rounded-[30px]"></div>
+      {/* Change style of the div depending on if elongated is true/false */}
+      {props.elongated ? (
+        <div className=""></div>
+      ) : (
+        <div className="w-32 h-32 bg-bggray rounded-[30px]"></div>
+      )}
     </>
   );
 };
