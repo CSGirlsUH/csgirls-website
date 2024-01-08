ab025f230a90f4deca9bda3becb9e68095360059
SageCT <99381480+SageCT@users.noreply.github.com>
2024-01-08 14:47:46 -0600

    renamed some variables in officermain.tsx

--- csgwebsite/src/components/OfficerCard.tsx
+++ csgwebsite/src/components/OfficerCard.tsx
@@ -1,16 +1,24 @@
 interface CardProps {
-  position: string;
   name: string;
+  position: string;
+  pic: string;
   discord: string;
   linkedin: string;
   email: string;
 }
 
-const OfficerCard = () => {
+const OfficerCard = (props: CardProps) => {
   return (
     <>
       {/* Mobile Variant */}
-      <div className="flex flex-col w-269 h-346 bg-bggray"></div>
+      <div className="flex flex-col w-[269px] h-[346px] bg-blue-800 rounded-[20px] ">
+        {/* Title */}
+        <h1 className=""></h1>
+        <h1 className="flex flex-row text-[20px] font-firacode justify-center pt-2">
+          {props.position}
+        </h1>
+        <h1>{props.name}</h1>
+      </div>
     </>
   );
 };
