343d686114f65f5f37640db203d3452dfbe0e0f5
SageCT <99381480+SageCT@users.noreply.github.com>
2024-01-28 17:08:31 -0600

    Feat: Adding loading for the API fetch

--- csgwebsite/src/components/UpEvents.tsx
+++ csgwebsite/src/components/UpEvents.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { gapi } from "gapi-script";
 import BigEventsCard from "./BigEventsCard";
 import SmallEventsCard from "./SmallEventsCard";
+import { set } from "react-hook-form";
 
 const UpEvents = () => {
   // ? Integration with Google Calendar API to auto generate this list
@@ -15,19 +16,7 @@ const UpEvents = () => {
   const TESTING_CALENDAR_ID =
     "a9019437cc41150804219b9139aaa6707fe341f3432b67d96271d85d30ad29e7@group.calendar.google.com";
   const [events, setEvents] = useState([]);
-
-  const items: [string, string[]][] = [
-    ["Oct 17", ["Lunch with Tim Apple", "Free T-Shirts for members!"]],
-    ["Oct 18", ["Lunch with Tim Apple", "Free T-Shirts for members!"]],
-    ["Oct 19", ["Lunch with Tim Apple", "Free T-Shirts for members!"]],
-    ["Oct 20", ["Lunch with Tim Apple", "Free T-Shirts for members!"]],
-    ["Oct 21", ["Lunch with Tim Apple", "Free T-Shirts for members!"]],
-    ["Oct 22", ["Lunch with Tim Apple", "Free T-Shirts for members!"]],
-    ["Oct 23", ["Lunch with Tim Apple", "Free T-Shirts for members!"]],
-    ["Oct 24", ["Lunch with Tim Apple", "Free T-Shirts for members!"]],
-    ["Oct 25", ["Lunch with Tim Apple", "Free T-Shirts for members!"]],
-    ["Oct 26", ["Lunch with Tim Apple", "Free T-Shirts for members!"]],
-  ];
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     gapi.load("client:auth2", async () => {
@@ -60,15 +49,18 @@ const UpEvents = () => {
         calendarId: calendarID,
         showDeleted: false,
         maxResults: 10,
-        timeMin: "2023-11-15T02:28:18+00:00",
+        timeMin: new Date().toISOString(),
+        // ! PLACEHOLDER TIME FOR TESTING, REMOVE FOR PRODUCTION
       });
       console.log(new Date().toISOString());
       console.log(response);
 
-      // Return events
+      // Return events and set Loading to false
+      setIsLoading(false);
       return response.result.items;
     } catch (error) {
       console.log("Failed to fetch events", error);
+      setIsLoading(false);
       return [];
     }
   };
@@ -151,8 +143,6 @@ const UpEvents = () => {
 
   eventItems.reverse();
 
-  console.log(eventItems);
-
   return (
     <>
       {/* Mobile Variant */}
@@ -163,15 +153,26 @@ const UpEvents = () => {
         <div className="flex md:hidden items-center justify-start overflow-x-scroll overflow-y-hidden">
           {/* TESTING GOOGLE MAPS CALLS */}
           <div className="py-4 flex items-center justify-start">
-            <SmallEventsCard
-              optional="ml-6"
-              date={items[0][0]}
-              items={items[0][1]}
-            />
-
-            {items.slice(1).map((item, index) => (
-              <SmallEventsCard key={index} date={item[0]} items={item[1]} />
-            ))}
+            {!isLoading ? (
+              <SmallEventsCard
+                optional="ml-6"
+                date={eventItems[0].date}
+                items={[eventItems[0].title]}
+              />
+            ) : (
+              <p>Loading...</p>
+            )}
+            {!isLoading
+              ? eventItems
+                  .slice(1)
+                  .map((item, index) => (
+                    <SmallEventsCard
+                      key={index}
+                      date={item.date}
+                      items={[item.title]}
+                    />
+                  ))
+              : null}
           </div>
         </div>
       </div>
@@ -184,16 +185,26 @@ const UpEvents = () => {
         {/* Events Carousel */}
         <div className="flex items-center justify-start overflow-x-scroll overflow-y-hidden ">
           <div className="py-4 flex items-center justify-start">
-            {eventItems && eventItems.length > 0 && (
+            {!isLoading ? (
               <BigEventsCard
                 optional="ml-6"
                 date={eventItems[0].date}
                 items={[eventItems[0].title]}
               />
+            ) : (
+              <p>Loading...</p>
             )}
-            {eventItems.slice(1).map((item, index) => (
-              <BigEventsCard key={index} date={item.date} items={item.title} />
-            ))}
+            {!isLoading
+              ? eventItems
+                  .slice(1)
+                  .map((item, index) => (
+                    <BigEventsCard
+                      key={index}
+                      date={item.date}
+                      items={[item.title]}
+                    />
+                  ))
+              : null}
           </div>
         </div>
       </div>
