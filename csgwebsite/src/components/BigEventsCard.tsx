7f324ea8fbfb7f10cd283c5c295d761cba3c19ab
SageCT <99381480+SageCT@users.noreply.github.com>
2024-03-02 17:22:04 -0600

    BigEventsCard.tsx reformat

--- csgwebsite/src/components/BigEventsCard.tsx
+++ csgwebsite/src/components/BigEventsCard.tsx
@@ -13,7 +13,7 @@ function BigEventsCard(props: CardProps) {
       {
         <div
           className={
-            "mr-[51px] flex min-h-[346px] min-w-[269px] flex-col rounded-[20px] bg-bggray" +
+            "mr-[51px] flex min-h-[20rem] min-w-[16rem] flex-col rounded-[20px] bg-bggray" +
             " " +
             props.optional
           }
@@ -21,9 +21,11 @@ function BigEventsCard(props: CardProps) {
           <h1 className="flex flex-col py-6 text-center text-[36px]">
             {props.date}
           </h1>
-          <ol className="list-group list-disc pl-6">
+          <ol className="list-group list-disc pl-0 pt-10">
             {props.items.map((item, index) => (
-              <li key={index}>{item}</li>
+              <li className="list-none px-2 text-center text-xl" key={index}>
+                {item}
+              </li>
             ))}
           </ol>
         </div>
