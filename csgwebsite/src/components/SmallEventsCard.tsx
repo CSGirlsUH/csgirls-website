2fa00ab6428f852ff9d74791cb1974faf8c6a4ff
SageCT <99381480+SageCT@users.noreply.github.com>
2024-04-04 16:01:25 -0500

    Added time to UpEvents on mobile

--- csgwebsite/src/components/SmallEventsCard.tsx
+++ csgwebsite/src/components/SmallEventsCard.tsx
@@ -1,5 +1,6 @@
 interface CardProps {
   date: string;
+  time?: string;
   items: string[];
   optional?: string;
 }
@@ -8,17 +9,22 @@ function SmallEventsCard(props: CardProps) {
   return (
     <div
       className={
-        "mr-[14px] flex h-[194px] min-w-[195px] flex-col justify-start rounded-[30px] bg-bggray pl-3 align-top" +
-        " " +
+        'mr-[14px] flex h-[194px] min-w-[195px] flex-col justify-start rounded-[30px] bg-bggray pl-3 align-top' +
+        ' ' +
         props.optional
       }
     >
-      <h1 className="flex self-center px-3 pb-1 pt-3 text-2xl">{props.date}</h1>
+      <h1 className="flex self-center pb-1 pt-3 text-2xl">{props.date}</h1>
       <ol className="flex list-disc flex-col gap-1 text-base">
         {props.items.map((item, index) => (
-          <li key={index} className="ml-6">
-            {item}
-          </li>
+          <>
+            <h3 className="font-poppins text-lg text-logopurple">
+              {props.time == 'N/A' ? 'TBA' : props.time}
+            </h3>
+            <li key={index} className="ml-4">
+              {item}
+            </li>
+          </>
         ))}
       </ol>
     </div>
