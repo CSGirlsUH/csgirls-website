8f5f6a852ddfbec276da3e3daeba567b8c410da0
SageCT <99381480+SageCT@users.noreply.github.com>
2024-04-04 16:11:46 -0500

    Added time to UpEvents on desktop

--- csgwebsite/src/components/BigEventsCard.tsx
+++ csgwebsite/src/components/BigEventsCard.tsx
@@ -2,36 +2,32 @@
 
 interface CardProps {
   date: string;
+  time: string;
   items: string[];
   optional?: string;
 }
 
 function BigEventsCard(props: CardProps) {
   return (
-    <>
-      {/* Change style of the div depending on if elongated is true/false */}
-      {
-        <div
-          className={
-            "mr-[51px] flex min-h-[20rem] w-[16rem] flex-col rounded-[20px] bg-bggray" +
-            " " +
-            props.optional
-          }
-        >
-          <h1 className="flex flex-col py-6 text-center text-[36px]">
-            {props.date}
-          </h1>
-          <ol className="list-group list-disc pl-0 pt-10">
-            {props.items.map((item, index) => (
-              <li className="list-none px-2 text-center text-xl" key={index}>
-                {item}
-              </li>
-            ))}
-          </ol>
-        </div>
-        // If no images are provided, default to text only
+    <div
+      className={
+        'mr-[51px] flex min-h-[20rem] w-[16rem] flex-col rounded-[20px] bg-bggray' +
+        ' ' +
+        props.optional
       }
-    </>
+    >
+      <h1 className="flex flex-col pt-6 text-center text-[36px]">
+        {props.date}
+      </h1>
+      <ol className="list-group list-disc pl-0 pt-2">
+        {props.items.map((item, index) => (
+          <li className="list-none pl-5 text-left text-xl" key={index}>
+            <h3 className="text-left text-2xl text-logopurple">{props.time}</h3>
+            {item}
+          </li>
+        ))}
+      </ol>
+    </div>
   );
 }
 
